3e19d15359c302a45c8a7bb408ddf5811c671fe3
André gois <andre.s.gois3@gmail.com>
2021-08-08 19:07:31 -0300

    fix folder tasklist

---
+++ Curso_JS_TS_básico_avançado_2021/14-Tasklist/src/components/Main.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import Form from './Form';
+import TarefasCom from './Tarefas';
+import './css/Main.css';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+    tarefas: [],
+    index: -1,
+  };
+
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    if (!tarefas) return;
+    this.setState({ tarefas });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+    if (tarefas === prevState.tarefas) return;
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefa = [...tarefas];
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefa, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefa[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefa],
+        index: -1,
+      });
+    }
+  }
+
+  /** Aula 8 */
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  }
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  }
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefa = [...tarefas];
+    novasTarefa.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefa],
+    });
+  }
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+    return (
+      <div className="main">
+        <h1>Lista de Tarefas</h1>
+
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          novaTarefa={novaTarefa}
+        />
+
+        <TarefasCom
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+          tarefas={tarefas}
+        />
+
+      </div>
+    );
+  }
+}
